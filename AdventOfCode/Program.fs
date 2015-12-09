eb502194c2c2fd64d57407f23091dd0cda492152
Michael Stoyke <searge@mojang.com>
2015-12-09 11:01:29 +0100

    Initial commit

---
+++ AdventOfCode/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+open day1
+open day2
+open day3
+open day4
+open day5
+open day6
+open day7
+open day8
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Day 1 part 1 -> %A" day1.final
+    printfn "Day 1 part 2 -> %A" day1.above
+    printfn "Day 2 part 1 -> %A" day2.order
+    printfn "Day 2 part 2 -> %A" day2.ribbon
+    printfn "Day 3 part 1 -> %A" day3.amount
+    printfn "Day 3 part 2 -> %A" day3.total
+
+    printfn "Day 4 processing skipped, slow code!"
+//slow    printfn "Day 4 part 1 -> %A" day4.mine
+//slow    printfn "Day 4 part 2 -> %A" day4.mine_more
+
+    printfn "Day 5 part 1 -> %A" day5.nice
+
+
+    printfn "Press any key to continue, any other key to quit."
+    Console.ReadKey(true) |> ignore
+    0 // return an integer exit code
---
+++ AdventOfCode/day4.fs
@@ -0,0 +1,26 @@
+﻿module day4
+
+
+open System;
+open System.Text;
+open System.Security.Cryptography;
+
+let input = "yzbqklnj";
+
+let encode (t:string) =
+    use md5 = MD5.Create()
+    md5.ComputeHash(Encoding.UTF8.GetBytes(t))
+
+let verify1 (b:byte[]) = b.[0] = 0uy && b.[1] = 0uy && b.[2] < 16uy
+
+let numbers = seq { 0 .. Int32.MaxValue }
+
+let find_for t v = numbers |> Seq.find (fun n -> encode (sprintf "%s%d" t n) |> v)
+
+// day 4 part 1
+let mine = find_for input verify1
+
+let verify2 (b:byte[]) = b.[0] = 0uy && b.[1] = 0uy && b.[2] = 0uy
+
+// day 4 part 2
+let mine_more = find_for input verify2
